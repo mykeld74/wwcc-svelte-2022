d742ba8189054124d39fac1956cf999c510fe0ab
Mike Daugherty <mikedaugherty@msdwebdesigns.com>
2022-01-18 14:21:08 -0700

    Update programs

--- src/data/programs.json.ts
+++ src/data/programs.json.ts
@@ -1,24 +1,12 @@
 export default {
 	programs: [
 		{
-			date: '8-01-21',
-			location: '/Program-08-01-21.pdf'
+			date: '01-09-22',
+			location: '/Program-01-09-22.pdf'
 		},
 		{
-			date: '7-25-21',
-			location: '/Program-07-25-21.pdf'
-		},
-		{
-			date: '7-18-21',
-			location: '/Program-07-18-21.pdf'
-		},
-		{
-			date: '7-11-21',
-			location: '/Program-07-11-21.pdf'
-		},
-		{
-			date: '7-04-21',
-			location: '/Program-07-04-21.pdf'
+			date: '01-16-22',
+			location: '/Program-01-16-22.pdf'
 		}
 	]
 };
