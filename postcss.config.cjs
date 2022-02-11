a480aaad3e50c26a5b5b9e8c2cec52adef46570f
Mike Daugherty <mikedaugherty@msdwebdesigns.com>
2022-02-11 08:10:01 -0700

    UPDATE configs and add cssnano

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,7 +1,17 @@
 const autoprefixer = require('autoprefixer');
+const cssnano = require('cssnano');
+
+const mode = process.env.NODE_ENV;
+const dev = mode === 'development';
 
 const config = {
-	plugins: [autoprefixer]
+	plugins: [
+		autoprefixer(),
+		!dev &&
+			cssnano({
+				preset: 'default'
+			})
+	]
 };
 
 module.exports = config;
