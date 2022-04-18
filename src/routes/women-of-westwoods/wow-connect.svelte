dd20f2b1bcbc9a80f7e1e0ff0ff3be4fbd9064a5
Mike Daugherty <mikedaugherty@msdwebdesigns.com>
2022-04-18 07:49:13 -0600

    update connect page

--- src/routes/women-of-westwoods/wow-connect.svelte
+++ src/routes/women-of-westwoods/wow-connect.svelte
@@ -8,19 +8,17 @@
       title,
       date,
       time,
+			location,
       body
 		}`;
 		const Events = await client.fetch(query);
 		return { props: { Events } };
 	}
 </script>
+
 <script>
-	import PortableText from '@portabletext/svelte'
+	import PortableText from '@portabletext/svelte';
 	export let Events;
-
-	
-
-	
 </script>
 
 <svelte:head>
@@ -35,23 +33,21 @@
 		topic. All gatherings will be held at Westwoods Community Church. Upcoming dates are:
 	</p>
 
-	<div>
-		{#each Events as {title, date, time, body}}
-		<h3>{title}</h3>
-		<p>{date}</p>
-		<p>{time}</p>
-		<PortableText
-			blocks={body}			
-		/>
-
+	<section class="eventContainer">
+		{#each Events as { title, date, time, location, body }}
+			<article class="eventCard">
+				<h3>{title}</h3>
+				<p class="dateTime"><strong>Date: </strong>{date}</p>
+				<p class="dateTime"><strong>Time: </strong>{time}</p>
+				<p class="location"><strong>Location: </strong>{location}</p>
+				<hr />
+				<PortableText blocks={body} />
+			</article>
 		{/each}
-
-	</div>
-
-	<h3>May 17th - WoW Connect Night. Watch for details!</h3>
+	</section>
 </div>
 
-<style>
+<style lang="scss">
 	.container {
 		width: 100%;
 		max-width: 1170px;
@@ -61,7 +57,31 @@
 	h3 {
 		margin: 0 0 5px 0;
 	}
-	ul {
-		margin-left: 5vw;
+	.eventContainer {
+		width: 100%;
+		display: grid;
+		grid-template-columns: repeat(auto-fit, minmax(400px, 1fr));
+		gap: 20px;
+	}
+	.eventCard {
+		border: var(--theme-colors-text) 2px solid;
+		border-radius: 10px;
+		padding: 20px;
+		h3 {
+			margin: -20px 0 15px -20px;
+			color: var(--theme-colors-top_bar);
+			background: var(--theme-colors-text);
+			width: calc(100% + 40px);
+			padding: 10px 20px;
+		}
+		.dateTime {
+			margin: 0 0 10px 0;
+		}
+		.location {
+			margin: 0 0 20px 0;
+		}
+	}
+	hr {
+		background: var(--theme-colors-text);
 	}
 </style>
