3fe6afb6d4c224004895ae2177cc737ca4e1c5ca
Mike Daugherty <mikedaugherty@msdwebdesigns.com>
2022-04-18 11:03:58 -0600

    udpate wow page

--- src/routes/women-of-westwoods/index.svelte
+++ src/routes/women-of-westwoods/index.svelte
@@ -1,5 +1,5 @@
 <script>
-	import Image from '$components/image.svelte';
+	import Modal from '$components/modal.svelte';
 </script>
 
 <svelte:head>
@@ -24,17 +24,29 @@
 		>
 	</p>
 
+	<p class="opportunity">We have opportunities to help!</p>
 	<p>
 		Life happens! We get to be the part of peoples happiest and hardest moments as we do life with
 		them. As a community, we want to be an on-going resource to help in life's big moments. We are
 		creating an on-going meal train crew sign up. If you would like to be someone to potentially
-		prepare and drop off a meal for someone in need, please add your info to this google form:
+		prepare and drop off a meal for someone in need, please add your info to this form:
+		<Modal>
+			<p slot="trigger" class="trigger">Click here to add your info.</p>
+
+			<div slot="content">
+				<iframe
+					class="mealTrain"
+					title="Meal Train Sign Up"
+					src="https://docs.google.com/forms/d/e/1FAIpQLScThg82dHBFrRqkVendoOj9_rs5L7iJtbl2Tjz5CVJmgDslmQ/viewform"
+					frameborder="0"
+					marginheight="0"
+					marginwidth="0"
+				/>
+			</div>
+		</Modal>
 	</p>
-	<a href="https://forms.gle/dpUAMn7w3Ngwgazg9" target="_blank" rel="noopener noreferrer">
-		<p>https://forms.gle/dpUAMn7w3Ngwgazg9</p>
-		<div class="mealtrainContainer"><Image source="mealtrainLogo" altTag="Meal Train Logo" /></div>
-	</a>
-	<div class="center">
+
+	<!-- <div class="center">
 		<h2>Easter is Coming (April 17th)</h2>
 		<p>But the Season of Lent leads us in and prepares us.</p>
 
@@ -66,17 +78,37 @@
 			enhance your experience with the blessings contained it, and you'll have it for the days
 			beyond.
 		</p>
-	</div>
+	</div> -->
 </div>
 
-<style>
+<style lang="scss">
 	.container {
 		width: 100%;
 		max-width: 1170px;
 		margin: 30px auto;
 		padding: 0 15px;
 	}
-	.mealtrainContainer {
-		width: 300px;
+
+	.welcome {
+		font-size: clamp(20px, 3.5vw, 36px);
+	}
+	.opportunity {
+		font-size: clamp(20px, 3vw, 26px);
+		font-weight: bold;
+	}
+
+	.trigger {
+		color: var(--linkColor);
+		font-weight: 600;
+		margin-top: 10px;
+		cursor: pointer;
+		transition: all 0.3s ease-in-out;
+		&:hover {
+			text-decoration: underline;
+		}
+	}
+	.mealTrain {
+		width: 100%;
+		aspect-ratio: 16/9;
 	}
 </style>
