e7b691a57edbd47ab6045385a6138f00a190733d
Mike Daugherty <mikedaugherty@msdwebdesigns.com>
2022-04-18 07:58:15 -0600

    update connect page

--- src/routes/women-of-westwoods/wow-connect.svelte
+++ src/routes/women-of-westwoods/wow-connect.svelte
@@ -60,7 +60,7 @@
 	.eventContainer {
 		width: 100%;
 		display: grid;
-		grid-template-columns: repeat(auto-fit, minmax(400px, 1fr));
+		grid-template-columns: repeat(auto-fit, minmax(350px, 1fr));
 		gap: 20px;
 	}
 	.eventCard {
