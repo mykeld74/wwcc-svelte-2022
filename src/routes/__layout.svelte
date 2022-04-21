d954021afd9255e29182423cad9fcf6dbd43bf5c
Mike Daugherty <mikedaugherty@msdwebdesigns.com>
2022-04-21 07:57:18 -0600

    move GA to the right place

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -18,6 +18,20 @@
 
 <svelte:head>
 	<title>{title}</title>
+	<!--  Start Google Analytics -->
+	<script>
+		window.ga = function () {
+			ga.q.push(arguments);
+		};
+		ga.q = [];
+		ga.l = +new Date();
+		ga('create', 'UA-120053347-1', 'auto');
+		ga('set', 'anonymizeIp', true);
+		ga('set', 'transport', 'beacon');
+		ga('send', 'pageview');
+	</script>
+	<script src="https://www.google-analytics.com/analytics.js" async></script>
+	<!--  End Google Analytics -->
 </svelte:head>
 
 <div id="primaryContainer" hidden>
--- src/routes/about-us/__layout.svelte
+++ src/routes/about-us/__layout.svelte
@@ -2,22 +2,7 @@
 	import { fade } from 'svelte/transition';
 </script>
 
-<svelte:head>
-	<!--  Start Google Analytics -->
-	<script>
-		window.ga = function () {
-			ga.q.push(arguments);
-		};
-		ga.q = [];
-		ga.l = +new Date();
-		ga('create', 'UA-120053347-1', 'auto');
-		ga('set', 'anonymizeIp', true);
-		ga('set', 'transport', 'beacon');
-		ga('send', 'pageview');
-	</script>
-	<script src="https://www.google-analytics.com/analytics.js" async></script>
-	<!--  End Google Analytics -->
-</svelte:head>
+<svelte:head />
 
 <div in:fade={{ duration: 750 }} out:fade={{ duration: 250 }}>
 	<slot />
