c6c15c443e664b4fbabc0bc9f66b760a4179d8ee
Mike Daugherty <mikedaugherty@msdwebdesigns.com>
2022-05-19 13:47:38 -0600

    add 5/22 program

--- src/data/programs.json.ts
+++ src/data/programs.json.ts
@@ -1,20 +1,8 @@
 export default {
 	programs: [
 		{
-			date: '05-08-22',
-			location: '/Program-05-08-22.pdf'
-		},
-		{
-			date: '05-01-22',
-			location: '/Program-05-01-22.pdf'
-		},
-		{
-			date: '04-24-22',
-			location: '/Program-04-24-22.pdf'
-		},
-		{
-			date: '04-17-22',
-			location: '/Program-04-17-22.pdf'
+			date: '05-22-22',
+			location: '/Program-05-22-22.pdf'
 		}
 	]
 };
