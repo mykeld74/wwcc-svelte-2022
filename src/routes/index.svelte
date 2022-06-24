16446f1fbb5eba244fdc9684cc93caa6256796ab
Mike Daugherty <mikedaugherty@msdwebdesigns.com>
2022-06-24 07:40:57 -0600

    add 6-26 program and remove kids camp

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -13,7 +13,6 @@
 	import WatchOnline from '$components/watchOnline.svelte';
 
 	let filled = false;
-	let kidsCampCheck = '1';
 
 	gsap.registerPlugin(ScrollTrigger);
 
@@ -120,31 +119,6 @@
 			</path>
 		</svg>
 	</div>
-</div>
-<div class="banner">
-	<Modal modalName="kidsCamp" isOpen={kidsCampCheck === '1' ? false : true}>
-		<p slot="trigger" class="trigger lbText">Adventure Island Kids Camp - Tuesday Nights in June</p>
-		<div slot="content">
-			<div class="aiImage"><Img source="adventureIsland" altTag="Adventure Island Logo" /></div>
-			<p>
-				Join us for a great summer of connection as we gather with kids and families for Kids
-				Connect. Happening every Tuesday evening for the month of June.
-			</p>
-			<p>We will have a free community meal each night from 5-5:30pm.</p>
-			<p>
-				Our programming will be from 5:30-7:30pm. There will be great time for learning, having fun
-				and growing together.
-			</p>
-			<p>
-				You can register here: <a
-					href="https://westwoodskids.mycokesburyvbs.com/"
-					target="_blank"
-					rel="noopener noreferrer">https://westwoodskids.mycokesburyvbs.com</a
-				>
-			</p>
-			<p>Open for kiddos 3 years old - 5th Grade</p>
-		</div>
-	</Modal>
 </div>
 
 <div id="linkSection">
@@ -342,15 +316,5 @@
 	#wwMap {
 		width: 100%;
 		height: 50vh;
-	}
-
-	.aiImage {
-		width: clamp(200px, 100%, 350px);
-		margin: auto;
-	}
-	.banner {
-		background: var(--red);
-		text-align: center;
-		cursor: pointer;
 	}
 </style>
--- src/routes/westwoods-kids/index.svelte
+++ src/routes/westwoods-kids/index.svelte
@@ -1,13 +1,5 @@
 <script>
-	import { onMount } from 'svelte';
 	import Image from '$components/image.svelte';
-	import Modal from '$components/modal.svelte';
-
-	let kidsCampCheck = '1';
-
-	onMount(() => {
-		kidsCampCheck = localStorage.getItem('wwkidsCamp');
-	});
 </script>
 
 <svelte:head>
@@ -21,37 +13,7 @@
 	<h1>Welcome to Westwoods Kids</h1>
 	<p>Transforming our Next Generation with the Love of Jesus One Friend at a Time</p>
 </div>
-<Modal modalName="wwkidsCamp" isOpen={kidsCampCheck === '1' ? false : true}>
-	<div class="wwKidCamp" slot="trigger">
-		<h3>Tuesdays in June:</h3>
-		<div class="bannerImage">
-			<Image source="adventureIsland" altTag="Adventure Island Logo" />
-		</div>
-		<h3>
-			Adventure Island Kids Camp<br />Click here to learn more!
-		</h3>
-	</div>
-	<div slot="content">
-		<div class="aiImage"><Image source="adventureIsland" altTag="Adventure Island Logo" /></div>
-		<p>
-			Join us for a great summer of connection as we gather with kids and families for Kids Connect.
-			Happening every Tuesday evening for the month of June.
-		</p>
-		<p>We will have a free community meal each night from 5-5:30pm.</p>
-		<p>
-			Our programming will be from 5:30-7:30pm. There will be great time for learning, having fun
-			and growing together.
-		</p>
-		<p>
-			You can register here: <a
-				href="https://westwoodskids.mycokesburyvbs.com/"
-				target="_blank"
-				rel="noopener noreferrer">https://westwoodskids.mycokesburyvbs.com</a
-			>
-		</p>
-		<p>Open for kiddos 3 years old - 5th Grade</p>
-	</div>
-</Modal>
+
 <div class="textBlock blue">
 	<div class="container"><h2>Westwoods Kids core values are:</h2></div>
 </div>
@@ -114,34 +76,5 @@
 		&.red {
 			background: #c21f48;
 		}
-	}
-	.aiImage {
-		width: clamp(200px, 100%, 350px);
-		margin: auto;
-	}
-	.wwKidCamp {
-		width: clamp(300px, 90%, 680px);
-		text-align: center;
-		background: #c21f48;
-		padding: 15px 0;
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-		justify-content: center;
-		margin: 30px auto;
-		border-radius: 80px;
-		transition: background 0.3s ease-in-out;
-		color: #fff;
-		&:hover {
-			background: #540d36;
-		}
-		h3 {
-			cursor: pointer;
-			margin: 0;
-		}
-	}
-	.bannerImage {
-		width: clamp(150px, 100%, 250px);
-		margin: 20px auto;
 	}
 </style>
