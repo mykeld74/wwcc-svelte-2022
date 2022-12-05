f805593ce139258055c8158e8b69032f8477ff1e
Mike Daugherty <mikedaugherty@msdwebdesigns.com>
2022-12-05 15:53:13 -0700

    fix order issue on WoW connect

--- src/routes/women-of-westwoods/wow-connect/+page.js
+++ src/routes/women-of-westwoods/wow-connect/+page.js
@@ -2,16 +2,16 @@ import client from '$lib/client';
 export const prerender = true;
 
 export async function load() {
-	const query = `*[_type == "event"] | order(order desc){
+	const query = `*[_type == "event"] | order(order asc){
       _id,
       title,
       date,
       time,
 			location,
       body,
+
 		}`;
 	const Events = await client.fetch(query);
 
-	console.log(Events);
 	return { Events };
 }
