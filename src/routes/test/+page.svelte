4e6f47a9207b8b704463ba674e680d2f249ddcdf
mykeld74 <mike@msdwebsolutions.com>
2024-09-13 10:45:44 -0600

    Trying to update cloudinary

--- src/routes/test/+page.svelte
+++ src/routes/test/+page.svelte
@@ -4,5 +4,8 @@
 
 <div class="container">
 	<h1>Test Page</h1>
-	<img src={Lion} alt="Lion" />
+	<img
+		src="https://res.cloudinary.com/mykeld74/image/upload/f_auto,q_auto,w_auto/WestwoodsCC/{Lion}"
+		alt="Lion"
+	/>
 </div>
